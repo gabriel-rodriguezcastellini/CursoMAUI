dd33ef3deb3e16558fd9e4b10a0d7dcf6fcfdcfb
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-09-01 19:30:48 -0300

    LABEL-VIEW

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿using CursoMAUI.Pages;
-
-namespace CursoMAUI;
+﻿namespace CursoMAUI;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-    }
-
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        FlyoutPageItem flyoutPageItem = new()
-        {
-            Title = "Navigation Pages Example",
-            IconSource = "contact.png"
-        };
-
-        await Navigation.PushAsync(new Page2()
-        {
-            BindingContext = flyoutPageItem
-        });
     }
 }
